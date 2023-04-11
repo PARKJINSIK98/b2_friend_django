0668901790fad84ddf00c3886c3c3d046d8a6454
PARKJINSIK98 <127724952+PARKJINSIK98@users.noreply.github.com>
2023-04-11 15:19:33 +0900

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    age = models.IntegerField(null=True)
+    gender = models.CharField(max_length=10)
+    region = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.user.username
