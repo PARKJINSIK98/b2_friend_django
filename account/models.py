70b93c38acd5c2fb6ac02773e32e8cf6444124f8
PARKJINSIK98 <127724952+PARKJINSIK98@users.noreply.github.com>
2023-04-11 12:07:31 +0900

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    gender_choices = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ]
+    gender = models.CharField(choices=gender_choices, max_length=10)
+    image = models.ImageField(upload_to='profile_images', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
