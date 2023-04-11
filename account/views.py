a38efe3a862b00fdccef195b8f377dc5e8745560
PARKJINSIK98 <127724952+PARKJINSIK98@users.noreply.github.com>
2023-04-11 15:20:11 +0900

    Update views.py

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib import messages
+from .forms import ProfileForm
 
-# Create your views here.
+@login_required
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '프로필이 업데이트되었습니다.')
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    context = {'form': form}
+    return render(request, 'account/profile.html', context)
